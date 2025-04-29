fa1ccc402a8374f63413c511b3c04a16ddaf0438
Lucas Honorio <lucashonorioo@hotmail.com>
2025-04-29 20:06:34 -0300

    Mudanças configuração

--- evento-celebrativo-api/src/main/java/com/eventoscelebrativos/Application.java
+++ evento-celebrativo-api/src/main/java/com/eventoscelebrativos/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiDeEventosCelebrativosApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiDeEventosCelebrativosApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
