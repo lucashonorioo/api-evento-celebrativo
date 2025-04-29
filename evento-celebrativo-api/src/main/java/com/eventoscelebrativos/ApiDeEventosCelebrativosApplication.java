10811d586c18e75ef8ca9f5869b3cb28952b994f
Lucas Honorio <lucashonorioo@hotmail.com>
2025-04-29 19:11:10 -0300

    Projeto criado

---
+++ evento-celebrativo-api/src/main/java/com/eventoscelebrativos/ApiDeEventosCelebrativosApplication.java
@@ -0,0 +1,13 @@
+package com.eventoscelebrativos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDeEventosCelebrativosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDeEventosCelebrativosApplication.class, args);
+	}
+
+}
---
+++ evento-celebrativo-api/src/test/java/com/eventoscelebrativos/ApiDeEventosCelebrativosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventoscelebrativos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDeEventosCelebrativosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
